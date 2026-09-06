1ea4e8e8a00a8a7cf67db015cb31638105fe0706
Pedro Cramer <pedrocramer.dev@proton.me>
2026-09-06 14:50:05 -0300

    feat: add screen switching

--- game.js
+++ game.js
@@ -13,7 +13,7 @@ const background = {
     height: 204,
     x: 0,
     y: canvasEl.height - 204,
-    draw: function() {
+    draw: function () {
         context.fillStyle = '#70c5ce';
         context.fillRect(0, 0, canvasEl.width, canvasEl.height);
 
@@ -41,7 +41,7 @@ const ground = {
     height: 112,
     x: 0,
     y: canvasEl.height - 112,
-    draw: function() {
+    draw: function () {
         context.drawImage(
             sprites,
             this.sourceX, this.sourceY,
@@ -68,11 +68,11 @@ const bird = {
     y: 50,
     gravity: 0.25,
     speed: 0,
-    update: function() {
+    update: function () {
         this.speed += this.gravity;
         this.y += this.speed;
     },
-    draw: function() {
+    draw: function () {
         context.drawImage(
             sprites,
             this.sourceX, this.sourceY,
@@ -83,14 +83,70 @@ const bird = {
     }
 };
 
-function loop() {
-    bird.update();
+const getReadyMessage = {
+    sourceX: 134,
+    sourceY: 0,
+    width: 174,
+    height: 152,
+    x: (canvasEl.width / 2) - (174 / 2),
+    y: 50,
+    draw: function () {
+        context.drawImage(
+            sprites,
+            this.sourceX, this.sourceY,
+            this.width, this.height,
+            this.x, this.y,
+            this.width, this.height
+        );
+    }
+};
 
-    background.draw();
-    ground.draw();
-    bird.draw();
+// Scenes
+
+let currentScene = {};
+function changeScene(newScene) {
+    currentScene = newScene;
+}
+
+const Scenes = {};
+
+Scenes.START = {
+    draw: function () {
+        background.draw();
+        ground.draw();
+        getReadyMessage.draw();
+        bird.draw();
+    },
+    click: function() {
+        changeScene(Scenes.GAME);
+    },
+    update: function() {}
+}
+
+Scenes.GAME = {
+    draw: function () {
+        background.draw();
+        ground.draw();
+        bird.draw();
+    },
+    update: function() {
+        bird.update();
+    }
+}
+
+function loop() {
+    currentScene.draw();
+    currentScene.update();
 
     requestAnimationFrame(loop);
 }
+
+canvasEl.addEventListener('click', () => {
+    if (currentScene.click) {
+        currentScene.click();
+    }
+});
+
+changeScene(Scenes.START);
 
 loop();
